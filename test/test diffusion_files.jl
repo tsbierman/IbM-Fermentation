4f5b7e3dc958cec210fc0a4faec8593c42b18a87
tsbierman <ts.bierman@gmail.com>
2023-12-04 15:13:42 +0100

    Added diffusion tests
    Added some tests for diffusion files. For some of the files, it does not make sense to do this as they require specific inputs.

--- test/test diffusion_files.jl
+++ test/test diffusion_files.jl
@@ -7,6 +7,9 @@ using DSP
 include(string(pwd(),"\\lib\\Lib_Module.jl"))
 include(string(pwd(), "\\lib\\diffusion\\prolongation.jl"))
 include(string(pwd(), "\\lib\\diffusion\\restriction.jl"))
+include(string(pwd(), "\\lib\\diffusion\\residual.jl"))
+include(string(pwd(), "\\lib\\diffusion\\smoothing.jl"))
+include(string(pwd(), "\\lib\\diffusion\\create_dirichlet_boundary.jl"))
 
 @testset "prolongation" begin
     phi_coarse = [1 5 3 6; 3 6 2 3; 4 2 2 3; 1 1 6 4]
@@ -23,4 +26,36 @@ end
     expected = [16 38 14; 45 57 34; 27 37 32] ./ 16
     result = restriction(phi_fine, L_restriction)
     @test expected == round.(result, digits=4)
+end
+
+@testset "create_dirichlet_boundary" begin
+    mock_matrix = ones(3,3)
+    mock_matrix1 = zeros(50,31)
+    value = 8
+    result = create_dirichlet_boundary(mock_matrix, value)
+    result1 = create_dirichlet_boundary(mock_matrix1, value)
+    @test size(result) == (5,5)
+    @test sum(result) == sum(mock_matrix) + (2 * size(mock_matrix, 1) + 2 * size(mock_matrix, 2) + 4) * value
+    @test all(result[2:end-1, 2:end-1] .== 1)
+    @test all(result[1,:] .== value)
+    @test all(result[:,1] .== value)
+    @test all(result[end,:] .== value)
+    @test all(result[:,end] .== value)
+
+    @test size(result1) == (52,33)
+    @test sum(result1) == sum(mock_matrix1) + (2 * size(mock_matrix1, 1) + 2 * size(mock_matrix1, 2) + 4) * value
+    @test all(result1[2:end-1, 2:end-1] .== 0)
+    @test all(result1[1,:] .== value)
+    @test all(result1[:,1] .== value)
+    @test all(result1[end,:] .== value)
+    @test all(result1[:,end] .== value)
+end
+
+@testset "residual" begin
+    phi = [1 5 3 6; 3 6 2 3; 4 2 2 3; 1 1 6 4] # Mock concentrations
+    L_lhs = [1 2 1; 2 4 2; 1 2 1] / 4          # Mock stencil
+    rhs = [3 5 2 9; 8 4 2 1; 0 3 2 5; 6 5 4 1]
+    expected_result = [-3.5 -6.25 -9.75 -0.5; -2.25 -10.5 -11 -8.75; -8.75 -8.5 -10 -4.5; 2 -2 -6.75 -8]
+    result = residual(phi, rhs, L_lhs)
+    @test round.(result,digits=2) == expected_result
 end
