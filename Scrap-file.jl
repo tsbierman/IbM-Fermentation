76d97f73de8620a6414fd58f2897e98679eac029
tsbierman <ts.bierman@gmail.com>
2023-12-04 13:52:49 +0100

    Added diffusionMG
    This function coordinates the diffusion multigrid solver

--- Scrap-file.jl
+++ Scrap-file.jl
@@ -43,3 +43,17 @@
 # g = parse(Int64, d[e[1]+1:end])
 # println(typeof(f))
 # --------------------------END PRIORITYQUEUE----------------------------
+
+# --------------START MULTIDIMENSIONAL BITARRAYS (FOR AFTER CONVOLUTION) -----------------
+# a = ones(4,4,2)
+# a[:,:,2] .= 2
+# b = [8, 6]
+# c = BitArray([0 0 0 0; 0 1 1 0; 1 0 0 1; 1 1 1 1])
+
+# special_b = reshape(b, 1,1,:)
+
+# special_c = repeat(c, outer = [1,1,2])
+
+# c .* a + .!special_c .* special_b
+
+# --------------START MULTIDIMENSIONAL BITARRAYS (FOR AFTER CONVOLUTION) -----------------
